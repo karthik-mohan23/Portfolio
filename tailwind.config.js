bfc498c1a1f76258a154bbf31d4b72c0cfb69d9c
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-08-15 16:57:32 +0530

    added custom font and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "bg-primary": "#f9f5f2",
+        "bg-secondary": "#ffffff",
+        "bg-tertiary": "#91a8ed",
+        "f-primary": "#282825",
+        "f-secondary": "#52514e",
+        "btn-yellow": "#f7cb45",
+        "btn-pink": "#ff91e7",
+        "btn-green": "#22a094",
+      },
+    },
   },
   plugins: [],
 };
