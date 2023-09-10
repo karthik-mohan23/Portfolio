27e08f40b21077b0bab10797b9cfa958347b6238
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-09-10 16:08:57 +0530

    changed spacing in Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -10,13 +10,13 @@ const Footer = () => {
       <div className="w-[90%] max-w-6xl mx-auto py-5">
         <div className="border rounded-full border-gray-300  "></div>
         <div className="flex justify-between flex-wrap py-10">
-          <div>
-            <h2 className="text-2xl font-medium mb-2">Karthik Mohan</h2>
+          <div className="mb-2 md:mb-0">
+            <h2 className="text-2xl font-medium  md:mb-2">Karthik Mohan</h2>
             <p className="font-medium text-xl">Full Stack Developer</p>
           </div>
           <div>
             <p className="mb-2">&copy; 2023 Karthik Mohan</p>
-            <div className="flex items-center justify-center gap-5 ">
+            <div className=" flex items-center md:justify-center gap-5 ">
               <AiOutlineGithub
                 size={25}
                 className="hover:cursor-pointer hover:-translate-y-1 duration-300"
